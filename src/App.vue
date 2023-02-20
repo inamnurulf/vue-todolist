794d9c9e067dfedf8493288c07cd51ab8a76b1c2
fiq <me@taufiqhdyt.com>
2023-02-20 11:04:49 +0700

    feat: add list view

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/list">List</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
